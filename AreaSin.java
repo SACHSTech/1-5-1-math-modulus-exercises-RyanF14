aa8045af9121ae5a62a387214fce425d6b6bbe7a
RyanF14 <ryan.fellin25@ycdsbk12.ca>
2024-02-18 11:04:05 -0500

    fixed trig issues

--- AreaSin.java
+++ AreaSin.java
@@ -10,7 +10,8 @@ class AreaSin extends ConsoleProgram {
     double SideA = readDouble("Input value of side A");
     double SideB = readDouble("Input value of Side B");
     double Angle = readDouble("Input the value of the angle in degrees");
-    double output = (SideA*SideB*Math.sin(Angle))/2;
+    double output = (SideA*SideB*Math.sin(Math.toRadians(Angle)))/2;
+ 
     System.out.println(output);   
   }
 }
--- Cos3rd.java
+++ Cos3rd.java
@@ -10,8 +10,9 @@ class Cos3rd extends ConsoleProgram {
     double Side1 = readDouble("Input the length of the first side");
     double Side2 = readDouble("Input the length of the second side");
     double angle = readDouble("Input the measurement of the angle in degrees");
-    double calcresult = Math.sqrt((Math.pow(Side1, 2) + (Math.pow(Side2 , 2) - (2*Side1*Side2*(Math.cos(angle))))));
-    System.out.println(calcresult);
+    double calcresult = ((Math.pow(Side1, 2) + (Math.pow(Side2 , 2) - (2*Side1*Side2*(Math.cos(Math.toRadians(angle)))))));
+    double squareroot = Math.sqrt(calcresult);
+    System.out.println(squareroot);
 
     
   }
